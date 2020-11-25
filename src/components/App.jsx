eb09ca2aefc5c297b4418ada08093b3bb125d856
Aniket Pathak <171230012@nitdelhi.ac.in>
2020-11-26 05:22:28 +0530

    Add Delete note functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  function handleClick(inputText, event) {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+    // setItems("");
+  }
+  function onDelete(id) {
+    setItems((prevItems) => {
+      return prevItems.filter(function (item, index) {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={handleClick} />
+      <Note note={{ title: "Hello", content: "This is content" }} />
+      {items.map(function (item, index) {
+        return <Note key={index} id={index} note={item} delete={onDelete} />;
+      })}
       <Footer />
     </div>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function Note(props) {
   return (
     <div className="note">
-      <h1>{props.title}</h1>
-      <p>{props.content}</p>
-      <button>DELETE</button>
+      <h1>{props.note.title}</h1>
+      <p>{props.note.content}</p>
+      <button onClick={() => props.delete(props.id)}>DELETE</button>
     </div>
   );
 }
